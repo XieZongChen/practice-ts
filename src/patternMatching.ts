f5043ca7d5ef2540a1ab0f5f3d1d5a6f11f57237
XieZongChen <952937570@qq.com>
2024-11-23 21:12:57 +0800

    feat: 新增笔记

--- src/patternMatching.ts
+++ src/patternMatching.ts
@@ -131,4 +131,16 @@
     ? Args
     : never;
   type GetParametersRes = GetParameters<(name: string, age: number) => void>;
+
+  /**
+   * 提取返回值的类型
+   * - Func 和模式类型做匹配，提取返回值到通过 infer 声明的局部变量 ReturnType 里返回
+   * - 参数类型可以是任意类型，也就是 any[]（注意，这里不能用 unknown，因为涉及到参数的逆变性质）
+   */
+  type GetReturnType<Func extends Function> = Func extends (
+    ...args: any[]
+  ) => infer ReturnType
+    ? ReturnType
+    : never;
+  type GetReturnTypeRes = GetReturnType<() => string>;
 })();
