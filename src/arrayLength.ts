de692b6eee681fb2f0a417abebfa0679706ff89c
XieZongChen <952937570@qq.com>
2025-01-05 16:13:50 +0800

    feat: 新增笔记

--- src/arrayLength.ts
+++ src/arrayLength.ts
@@ -93,4 +93,27 @@
     ? StrLen<Rest, [...CountArr, unknown]>
     : CountArr['length'];
   type StrLenRes = StrLen<'test'>;
+
+  /**
+   * 比较第一个数是否比第二个大
+   * - 类型参数 Num1 和 Num2 是待比较的两个数
+   * - 类型参数 CountArr 是计数用的，会不断累加，默认值是 [] 代表从 0 开始
+   * - 如果 Num1 extends Num2 成立，代表相等，直接返回 false
+   * - 否则判断计数数组的长度，如果先到了 Num2，那么就是 Num1 大，返回 true
+   * - 反之，如果先到了 Num1，那么就是 Num2 大，返回 false
+   * - 如果都没到就往计数数组 CountArr 中放入一个元素，继续递归
+   */
+  type GreaterThan<
+    Num1 extends number,
+    Num2 extends number,
+    CountArr extends unknown[] = []
+  > = Num1 extends Num2
+    ? false
+    : CountArr['length'] extends Num2
+    ? true
+    : CountArr['length'] extends Num1
+    ? false
+    : GreaterThan<Num1, Num2, [...CountArr, unknown]>;
+  type GreaterThanRes1 = GreaterThan<5, 6>;
+  type GreaterThanRes2 = GreaterThan<6, 5>;
 })();
